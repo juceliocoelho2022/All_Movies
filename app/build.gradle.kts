8caf564e7e9e45d13b0233e2de91ff1738fb14fd
Jucelio Farias Coelho <juceliocoelho2022@gmail.com>
2024-09-03 20:38:43 -0300

    button_learn_more.xml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,8 +1,13 @@
 plugins {
-    alias(libs.plugins.android.application)
-    alias(libs.plugins.kotlin.android)
+    id("com.android.application")
+    kotlin("android")
+    kotlin("kapt") // Adiciona o plugin do kapt
 }
 
+kapt {
+    correctErrorTypes = true
+    generateStubs = true
+}
 android {
     namespace = "com.devjucelio.allmovies"
     compileSdk = 34
@@ -25,6 +30,7 @@ android {
                 "proguard-rules.pro"
             )
         }
+
     }
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
@@ -33,7 +39,11 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    buildFeatures {
+        dataBinding = true
+    }
 }
+kapt { correctErrorTypes = true}
 
 dependencies {
 
@@ -60,6 +70,37 @@ dependencies {
         implementation ("com.google.android.gms:play-services-ads:22.0.0")
         implementation("com.squareup.retrofit2:retrofit:2.11.0")
         implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
+
+
+    implementation ("org.jetbrains.kotlin:kotlin-stdlib-jdk7: 1.7.0")
+    implementation ("androidx.appcompat:appcompat:1.0.2")
+    implementation ("androidx.core:core-ktx:1.0.2")
+    implementation ("androidx.constraintlayout:constraintlayout:1.1.3")
+    implementation ("com.google.android.material:material:1.0.0")
+    implementation ("androidx.vectordrawable:vectordrawable:1.0.1")
+
+    implementation ("androidx.lifecycle:lifecycle-extensions:2.2.0")
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.2.0")
+
+    // Dagger 2
+    val dagger_version ="2.28"
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.7.10")
+    implementation("com.google.dagger:dagger:2.38.1")
+    kapt("com.google.dagger:dagger-compiler:2.38.1")
+
+    //Firebase
+    implementation ("com.google.firebase:firebase-analytics:17.2.1")
+    implementation ("com.google.firebase:firebase-messaging:20.1.0")
+    implementation ("com.crashlytics.sdk.android:crashlytics:2.10.1")
+
+    implementation ("androidx.legacy:legacy-support-v4:1.0.0")
+    androidTestImplementation ("androidx.test.ext:junit:1.1.1")
+    androidTestImplementation ("androidx.test:runner:1.2.0")
+    androidTestImplementation ("androidx.test:rules:1.2.0")
+    androidTestImplementation ("androidx.test:core:1.2.0")
+    androidTestImplementation ("androidx.test.espresso:espresso-core:$rootProject.espressoVersion")
+    androidTestImplementation ("androidx.test.espresso:espresso-contrib:$rootProject.espressoVersion")
+    implementation ("androidx.test.espresso:espresso-idling-resource:$rootProject.espressoVersion")
 
     //Mockito
     testImplementation ("org.mockito:mockito-core:3.3.3")
@@ -88,6 +129,7 @@ dependencies {
         //Gson
         implementation  ("com.squareup.retrofit2:converter-gson:2.9.0")
          implementation ("com.squareup.retrofit2:converter-moshi:2.9.0")
+
         //Okhttp3
         implementation ("com.squareup.okhttp3:okhttp:5.0.0")
         implementation ("com.squareup.okhttp3:logging-interceptor:5.0.0")
